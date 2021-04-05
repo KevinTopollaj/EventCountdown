990a2d0ba0313b10ed4ef12db3a9e402b58f54b4
Kevin Topollaj <kevintopollaj@gmail.com>
2021-04-05 18:34:54 +0200

    Create AddEventViewController and in Main

---
+++ EventsCountdown/EventsCountdown/Controllers/AddEventViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddEventViewController.swift
+//  EventsCountdown
+//
+//  Created by Kevin Topollaj on 5.4.21.
+//
+
+import UIKit
+
+class AddEventViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
